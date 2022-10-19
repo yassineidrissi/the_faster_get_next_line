4cbb809862af1abfc8d371f38782ebf12f702faf
yassine idrissi <yassine42idrissi@gmail.com>
2022-10-19 17:32:44 +0100

    what happing

--- get_next_line.c
+++ get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: yaidriss <yaidriss@student1337.com>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/19 09:48:53 by yaidriss          #+#    #+#             */
-/*   Updated: 2022/10/19 16:18:31 by yaidriss         ###   ########.fr       */
+/*   Updated: 2022/10/19 16:22:28 by yaidriss         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,6 +19,7 @@ t_get	my_read(int fd, char *buffer, int a)
 
 	j = 1;
 	yi.data = "";
+	yi.end = 1;
 	if (!a)
 	{
 		yi.data = ft_calloc(BUFFER_SIZE + 1, 1);
@@ -37,7 +38,7 @@ t_get	my_read(int fd, char *buffer, int a)
 				{
 					yi.line = buffer;
 					buffer = NULL;
-					yi.end = 1;
+					yi.end = 0;
 				}
 			}
 		}
@@ -62,7 +63,7 @@ char	*get_next_line(int fd)
 	if(my_ft_strchr(buffer))
 		exist = 1;
 	final = my_read(fd,	buffer, exist);
-	if (final.line == NULL || !final.end--)
+	if (final.line == NULL || !final.end)
 		return (NULL);
 	buffer = final.next;
 	return (final.line);
